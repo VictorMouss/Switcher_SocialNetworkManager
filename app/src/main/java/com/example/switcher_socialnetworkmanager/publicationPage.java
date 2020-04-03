ad09dd116a5320a8907a8d2969d41383e2f359f9
VictorMouss <VictorMouss@users.noreply.github.com>
2020-04-03 19:14:29 +0200

    Version du 3/04

---
+++ app/src/main/java/com/example/switcher_socialnetworkmanager/publicationPage.java
@@ -0,0 +1,14 @@
+package com.example.switcher_socialnetworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class publicationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publication_page);
+    }
+}
