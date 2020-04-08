5744974d189b8d9d141dff6703935b4469a99c99
VictorMouss <VictorMouss@users.noreply.github.com>
2020-04-08 17:18:43 +0200

    Version 2 du 8/04

---
+++ app/src/main/java/com/example/switcher_socialnetworkmanager/parametre_page.java
@@ -0,0 +1,14 @@
+package com.example.switcher_socialnetworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parametre_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametre_page);
+    }
+}
