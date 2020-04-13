c94c972a5eea18d933d118af116b20f99d29dcb6
VictorMouss <VictorMouss@users.noreply.github.com>
2020-04-13 17:20:37 +0200

    Version 1 du 13/04 Changements graphiques

--- app/src/main/java/com/example/switcher_socialnetworkmanager/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/switcher_socialnetworkmanager/ScreenSlidePageFragment.java
@@ -20,53 +20,29 @@ import androidx.viewpager2.widget.ViewPager2;
 public class ScreenSlidePageFragment extends Fragment {
 
     int pageNumber;
-    Button btn_publi;
-    Button btn_settings;
-    Button btn_account;
-    private ViewPager2 viewPager;
 
-    public ScreenSlidePageFragment(int pageNumber, ViewPager2 viewPager) {
+    public ScreenSlidePageFragment(int pageNumber) {
         this.pageNumber = pageNumber;
-        this.viewPager = viewPager;
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View myView;
-        if (pageNumber == 0) {
-            myView = inflater.inflate(R.layout.activity_connexion_page, container, false);
-        } else if (pageNumber == 1) {
-            myView = inflater.inflate(R.layout.activity_publication_page, container, false);
-        } else {
-            myView = inflater.inflate(R.layout.activity_parametre_page, container, false);
+        switch (pageNumber) {
+            case 0:
+                myView = inflater.inflate(R.layout.connexion_page, container, false);
+                break;
+            case 1 :
+                myView = inflater.inflate(R.layout.publication_page, container, false);
+                break;
+            case 2 :
+                myView = inflater.inflate(R.layout.parametre_page, container, false);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + pageNumber);
         }
 
-        btn_publi = (Button) myView.findViewById(R.id.btn_publi);
-        btn_settings = (Button) myView.findViewById(R.id.btn_settings);
-        btn_account = (Button) myView.findViewById(R.id.btn_account);
-
-        btn_account.setOnClickListener(new View.OnClickListener() {
-
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(0);
-            }
-        });
-
-        btn_publi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(1);
-            }
-        });
-        btn_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(2);
-            }
-        });
         return myView;
     }
 
--- app/src/main/java/com/example/switcher_socialnetworkmanager/publication_page.java
+++ app/src/main/java/com/example/switcher_socialnetworkmanager/publication_page.java
@@ -1,30 +1,39 @@
 package com.example.switcher_socialnetworkmanager;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnDragListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class publication_page extends FragmentActivity implements View.OnClickListener {
     /**
+     *
      * The number of pages (wizard steps) to show in this demo.
      */
-    private static final int NUM_PAGES = 3;
 
+    private static final int NUM_PAGES = 3;
+    Button btn_publi;
+    Button btn_settings;
+    Button btn_account;
     /**
      * The pager widget, which handles animation and allows swiping horizontally to access previous
      * and next wizard steps.
      */
     private ViewPager2 viewPager;
-    Button btn_publi;
-    Button btn_settings;
-    Button btn_account;
-
     /**
      * The pager adapter, which provides the pages to the view pager widget.
      */
@@ -33,21 +42,60 @@ public class publication_page extends FragmentActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen_slide);
+        setContentView(R.layout.bottom_buttons);
 
         // Instantiate a ViewPager2 and a PagerAdapter.
-
         pagerAdapter = new ScreenSlidePagerAdapter(this);
         viewPager = findViewById(R.id.pager);
         viewPager.setAdapter(pagerAdapter);
         viewPager.setCurrentItem(1);
-        btn_account = (Button) findViewById(R.id.btn_account);
-        btn_publi = (Button) findViewById(R.id.btn_publi);
-        btn_settings = (Button) findViewById(R.id.btn_settings);
+        btn_account = findViewById(R.id.btn_account);
+        btn_publi = findViewById(R.id.btn_publi);
+        btn_settings = findViewById(R.id.btn_settings);
+
+
+
+        btn_account.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                Log.i("Bouton", "Btn_account cliqué");
+                viewPager.setCurrentItem(0);
+                btn_account.setForeground(getResources().getDrawable(R.drawable.bouton_account_small_down));
+                btn_publi.setForeground(getResources().getDrawable(R.drawable.bouton_publi_small));
+                btn_settings.setForeground(getResources().getDrawable(R.drawable.bouton_settings_small));
+            }
+        });
+
+        btn_publi.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                Log.i("Bouton", "Btn_publi cliqué");
+                viewPager.setCurrentItem(1);
+                btn_account.setForeground(getResources().getDrawable(R.drawable.bouton_account_small));
+                btn_publi.setForeground(getResources().getDrawable(R.drawable.bouton_publi_small_down));
+                btn_settings.setForeground(getResources().getDrawable(R.drawable.bouton_settings_small));
+            }
+        });
+
+        btn_settings.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                Log.i("Bouton", "Btn_settings cliqué");
+                viewPager.setCurrentItem(2);
+                btn_account.setForeground(getResources().getDrawable(R.drawable.bouton_account_small));
+                btn_publi.setForeground(getResources().getDrawable(R.drawable.bouton_publi_small));
+                btn_settings.setForeground(getResources().getDrawable(R.drawable.bouton_settings_small_down));
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
+        Log.i("Back", "BackPressed");
+
         if (viewPager.getCurrentItem() == 1) {
             // If the user is currently looking at the first step, allow the system to handle the
             // Back button. This calls finish() on this activity and pops the back stack.
@@ -64,7 +112,6 @@ public class publication_page extends FragmentActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-
     }
 
     /**
@@ -73,18 +120,21 @@ public class publication_page extends FragmentActivity implements View.OnClickLi
      */
     private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
 
+
         public ScreenSlidePagerAdapter(FragmentActivity fa) {
             super(fa);
         }
 
         @Override
         public ScreenSlidePageFragment createFragment(int position) {
-            return new ScreenSlidePageFragment(position, viewPager);
+            return new ScreenSlidePageFragment(position);
         }
 
         @Override
         public int getItemCount() {
+            Log.i("createFrag","created1");
             return NUM_PAGES;
         }
+
     }
 }
